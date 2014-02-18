d9cebc5bf097e32a910c5008f8e562d226f1c1ed
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-18 18:08:36 +0000

    puts method

--- makers.rb
+++ makers.rb
@@ -1,4 +1,11 @@
+#Creating and Executing Ruby Files
 people = "Makers"
 puts "THe people variable is currently: " + people
 people = "Coders"
 puts "The people variable is currently: " + people
+
+#2 puts Method same output
+puts "Hello, world"
+puts ("Hello, world")
+
+#
