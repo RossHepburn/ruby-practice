f01b36a5725179290b9b5ab6c45cd4b809337641
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-18 18:30:16 +0000

    Commenting

--- makers.rb
+++ makers.rb
@@ -1,11 +1,13 @@
-#Creating and Executing Ruby Files
+# Creating and Executing Ruby Files
+
+# Assign the string "Makers" to a variable and print it out
 people = "Makers"
 puts "THe people variable is currently: " + people
+
+# Change the variable 
 people = "Coders"
 puts "The people variable is currently: " + people
 
-#2 puts Method same output
+# 2 puts methods same output
 puts "Hello, world"
 puts ("Hello, world")
-
-#
